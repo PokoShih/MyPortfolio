ad6207e348b7cb4e2761a855207eec9d9dd796a0
Porkchopsss <pokoshih@gmail.com>
2020-11-25 01:19:48 +1100

    initial updload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import backImage from './assets/create.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      background: `url(${backImage})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    }}
+    >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +26,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="App-content">
+        what's up
+      </div>
     </div>
   );
 }
