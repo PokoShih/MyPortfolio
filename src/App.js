d34e1d0c21e5330b53de3f9dd66f33dc6414c437
Porkchopsss <pokoshih@gmail.com>
2020-11-30 18:47:12 +1100

    updates on header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import './App.css';
 import backImage from './assets/create.jpg';
-import headerNav from './components/headerNav.js';
-import { BrowserRouter as Router} from "react-router-dom";
+// import { BrowserRouter as Router} from "react-router-dom";
 //   BrowserRouter as Router,
 //   Switch,
 //   Route,
 //   Link,
 // } from "react-router-dom";
+import HeaderNav from './components/headerNav';
 
-function App() {
+export default function App() {
   return (
       <div className="App"
       style={{
       background: `url(${backImage})`,
       backgroundPosition: 'center',
       backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      opacity:0.5
+      backgroundRepeat: 'no-repeat'
     }}
     >
-        <headerNav className="App-header"/>
-      <div className="App-content">
-        what's up
+        <HeaderNav className="App-header"/>
+        <div className="App-content">
+          what's up
+        </div>
       </div>
-    </div>
   );
 }
-
-export default App;
--- src/components/headerNav.js
+++ src/components/headerNav.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-function headerNav(){
-    return <h1>Hi Poko</h1>
-}
+export default function HeaderNav() {
+    
+        var name = "Poko Shih";
+        var description = "Currently working at Coles full time as a manager, looking to change in career direction as a full stack web developer";
+        var city = "Melbourne Australia";
+    return (
+        <header id="home">
 
-export default headerNav
+            <nav id="nav-wrap">
+{/* 
+                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a> */}
+
+                <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                </ul>
+
+            </nav>
+
+            <div className="row banner">
+                <div className="banner-text">
+                    <h1 className="responsive-headline">My name is {name}.</h1>
+                    <h3>I'm based in {city}. {description}.</h3>
+                    <hr />
+                </div>
+            </div>
+
+            <p className="scrolldown">
+                <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
+
+        </header>
+    )
+}
